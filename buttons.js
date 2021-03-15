bb4f22823d6646cb3da2f54c93ef40f85e03c8c1
matthew-french <matthew.french@gmail.com>
2021-03-15 17:05:44 +0000

    buttons comments

--- buttons.js
+++ buttons.js
@@ -1,5 +1,8 @@
+// Get all the html content so no need to query the dom anymore.
 const buttons = Array.from(document.getElementsByClassName("button"));
 
+// Map because I love how tidy it looks. If I was worried about browser compatibility
+// the scipt would be pushed through babel. With huge data set your best bet is reverse for.
 buttons.map((elem, index) => {
     elem.innerHTML = index;
     elem.style.backgroundColor = index % 2 ? "red" : "green";
